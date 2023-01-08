769cc2089753fce840aaf3607b923109e6044b0a
zouxiaoliang <295919168@qq.com>
2023-01-08 14:57:36 +0800

    实现
    * 增加部分功能接口
    * 针对basic_auth方式进行提示
    * 增加登陆标记处理

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use std::error::Error;
 #[derive(Debug)]
 struct SHellContext {
     pub accont: Option<octocrab::Octocrab>,
+    pub login: bool,
 }
 
 impl SHellContext {
     fn new() -> Self {
-        Self { accont: None }
+        Self { accont: None, login: false }
     }
 }
 
@@ -18,42 +19,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut shell = Shell::new(SHellContext::new(), "> ");
 
     // Add some command
-    shell.commands.insert("login", Command::new("login into github".to_string(), login));
+    shell.commands.insert("login", Command::new("login {token}".to_string(), login));
     shell.commands.insert("delete", Command::new("delete github repos".to_string(), delete_repos));
     shell.commands.insert("repos", Command::new("list repos".to_string(), list_repos));
-
+    shell.commands.insert("stars", Command::new("list stars".to_string(), list_stars));
+    shell.commands.insert("fork", Command::new("fork user/repo".to_string(), fork_repo));
     // Run the shell
     shell.run()?;
 
     Ok(())
 }
 
-fn delete_repos(_context: &mut SHellContext, _args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    Ok(())
-}
-
-fn login(context: &mut SHellContext, args: Vec<String>) -> Result<(), Box<dyn Error>> {
-    if args.is_empty() {
-        let token = std::env::var("GITHUB_TOKEN")?;
-        context.accont = Some(octocrab::Octocrab::builder().personal_token(token).build()?);
-    } else if args.len() == 2 {
-        context.accont =
-            Some(octocrab::Octocrab::builder().personal_token(args[1].to_string()).build()?);
-    } else if args.len() == 3 {
-        context.accont = Some(
-            octocrab::Octocrab::builder()
-                .basic_auth(args[1].to_string(), args[2].to_string())
-                .build()?,
-        );
-    } else {
-        println!(" [error] args error, must be 'login username token'");
+fn delete_repos(context: &mut SHellContext, _args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    if !context.login {
         return Ok(());
     }
 
     Ok(())
 }
 
+fn login(context: &mut SHellContext, args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    let args = args[1..].to_vec();
+    if args.is_empty() {
+        let token = std::env::var("GITHUB_TOKEN")?;
+        context.accont = Some(octocrab::Octocrab::builder().personal_token(token).build()?);
+    } else if args.len() == 1 {
+        context.accont =
+            Some(octocrab::Octocrab::builder().personal_token(args[0].to_string()).build()?);
+    } else if args.len() == 2 {
+        let via = "GitHub has discontinued password authentication to the API starting on \
+         November 13, 2020 for all GitHub.com accounts, including those on a GitHub Free, \
+         GitHub Pro, GitHub Team, or GitHub Enterprise Cloud plan. You must now \
+         authenticate to the GitHub API with an API token, such as an OAuth access token, \
+         GitHub App installation access token, or personal access token, depending on what \
+         you need to do with the token.";
+        println!(" [error] {}", via);
+        return Ok(());
+    } else {
+        println!(" [error] {}", "args error, must be 'login {token}'");
+        return Ok(());
+    }
+
+    context.login = true;
+    Ok(())
+}
+
 fn list_repos(context: &mut SHellContext, args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    if !context.login {
+        return Ok(());
+    }
+
     if args.is_empty() {}
     let feature = async {
         let mut i = 0;
@@ -67,6 +82,11 @@ fn list_repos(context: &mut SHellContext, args: Vec<String>) -> Result<(), Box<d
                 .page(i)
                 .send()
                 .await;
+
+            if let Err(msg) = repos {
+                println!("get repos failed, what: {}", msg);
+                break;
+            }
 
             let mut repos = repos.unwrap();
             let items = repos.take_items();
@@ -88,5 +108,16 @@ fn list_repos(context: &mut SHellContext, args: Vec<String>) -> Result<(), Box<d
     };
     tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap().block_on(feature);
 
+    Ok(())
+}
+
+fn list_stars(_context: &mut SHellContext, _args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+fn fork_repo(context: &mut SHellContext, _args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    if !context.login {
+        return Ok(());
+    }
     Ok(())
 }
